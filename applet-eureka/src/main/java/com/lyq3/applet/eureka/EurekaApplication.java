435a75b93fa6dd00603fe9f6bce164b910c6cbc9
lyq3 <99190092@qq.com>
2018-06-04 22:31:36 +0800

    注册中心

---
+++ applet-eureka/src/main/java/com/lyq3/applet/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.lyq3.applet.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心启动类
+ * @author 卡卢比
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
