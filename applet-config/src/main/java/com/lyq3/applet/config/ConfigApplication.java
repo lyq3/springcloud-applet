9a2623e414843e8750f7e868b7435efff97fc278
lyq3 <99190092@qq.com>
2018-07-10 23:27:27 +0800

    配置config子目录，后面会改成/{application}现在是/{lable}

--- applet-config/src/main/java/com/lyq3/applet/config/ConfigApplication.java
+++ applet-config/src/main/java/com/lyq3/applet/config/ConfigApplication.java
@@ -2,6 +2,7 @@ package com.lyq3.applet.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * @author 卡卢比
  */
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigApplication {
     public static void main(String[] args) {
