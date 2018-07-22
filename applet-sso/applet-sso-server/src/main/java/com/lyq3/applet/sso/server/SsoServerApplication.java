c0fc57d0ae7eba01f12714b43d765b6f6877fef1
lyq3 <99190092@qq.com>
2018-07-22 18:59:05 +0800

    User

--- applet-sso/applet-sso-server/src/main/java/com/lyq3/applet/sso/server/SsoServerApplication.java
+++ applet-sso/applet-sso-server/src/main/java/com/lyq3/applet/sso/server/SsoServerApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.lyq3.applet.sso.server.mapper**")
-public class Application {
+public class SsoServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(SsoServerApplication.class,args);
     }
 }
