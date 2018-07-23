780960e5e0914a606c3fe61fab32885d25359934
lyq3 <99190092@qq.com>
2018-07-23 20:12:39 +0800

    地址常量类

---
+++ applet-sso/applet-sso-common/src/main/java/com/lyq3/applet/sso/common/constant/PathConstant.java
@@ -0,0 +1,14 @@
+package com.lyq3.applet.sso.common.constant;
+
+/**
+ * @author 卡卢比
+ * @createTime 2018年07月23日 20:09
+ * @description 地址常量类
+ */
+public class PathConstant {
+    /**注销登录地址*/
+    private static final String LOGOUT_URL = "/sso/logout";
+    /**登录地址*/
+    private static final String LOGIN_URL = "/sso/login";
+
+}
